768b090da4b9d1271e0baf51082a8903d3413d0e
Owen Palmer <speedcubegeek@gmail.com>
2023-03-10 02:09:07 -0800

    Add basic level map generated from .vox file. Add glb overlay with textures. Add player glb that rotates to the nearest 90 degree angle

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,23 @@
 use ambient_api::{
-    global::{time, frametime, State, Quat},
+    global::{time, frametime, Quat},
     components::core::{
+        model::{model_from_url},
         game_objects::player_camera,
         player::{player, user_id},
         primitives::{cube},
         transform::{lookat_center, translation, rotation, scale},
-        rendering::color,
+        rendering::{color, sky, sun},
     },
     player::KeyCode,
     concepts::{make_transformable, make_perspective_infinite_reverse_camera},
     prelude::*,
-    rand,
 };
-use components::{timer, voxel_update, voxel_direction, player_camera_ref, player_camera_yaw, player_follower};
+use components::{timer, voxel_update, voxel_direction, player_camera_ref, player_camera_yaw, player_follower, has_gravity, player_mesh_ref};
 use concepts::{make_voxel};
 use std::cell::{RefCell};
+use vox_format;
+
+const VOX_DATA: &[u8] = include_bytes!("../assets/level.vox");
 
 #[derive(Debug)]
 struct VoxelMap {
@@ -56,38 +59,34 @@ impl VoxelMap {
     }
 }
 
-#[main]
-pub async fn main() -> EventResult {
-    let voxel_world = RefCell::new(VoxelMap::new(vec![40,40,40]));
+fn load_level() {
+    make_transformable().with_default(sun()).spawn();
+    make_transformable().with_default(sky()).spawn();
 
     Entity::new()
         .with_merge(make_transformable())
-        .with(translation(), vec3(19.5,19.5,-1.))
-        .with_default(cube())
-        .with(scale(), vec3(40.,40.,1.))
-        .with(color(), vec4(0.1, 0.1, 0.1, 1.))
+        .with(scale(), Vec3::ONE * 10.)
+        .with(model_from_url(), asset_url("assets/level.glb").unwrap())
         .spawn();
 
-    for x in 10..30 {
-        for y in 10..30 {
+    let chunks = vox_format::from_slice(VOX_DATA).expect("Could not get voxel data");
+    for model in chunks.models {
+        for voxel in model.voxels {
+            let point = voxel.point;
             make_voxel()
                 .with_merge(make_transformable())
-                .with(translation(), vec3(x as f32, y as f32, 1.0))
-                .with_default(cube())
+                .with(translation(), vec3(point.x as f32, point.y as f32, point.z as f32))
+                // .with_default(cube())
                 .with(color(), vec4(1., 0.9, 0., 1.))
                 .spawn();
         }
     }
-    for x in 12..25 {
-        for y in 11..22 {
-            make_voxel()
-                .with_merge(make_transformable())
-                .with(translation(), vec3(x as f32, y as f32, 1.0))
-                .with_default(cube())
-                .with(color(), vec4(0.5,1.,0.1, 1.0))
-                .spawn();
-        }
-    }
+}
+
+#[main]
+pub async fn main() -> EventResult {
+    let voxel_world = RefCell::new(VoxelMap::new(vec![40,40,40]));
+    load_level();
 
     spawn_query((player(), user_id())).bind(move |players| {
         for (id, (_, user)) in players {
@@ -97,19 +96,28 @@ pub async fn main() -> EventResult {
                 .with_default(player_camera_yaw())
                 .with(user_id(), user)
                 .with(translation(), vec3(14.5, 14.5, 10.))
-                .with_default(rotation())
-                .with(lookat_center(), vec3(0.,0.,1.))
+                // .with_default(rotation())
+                .with(lookat_center(), vec3(39., 39., 1.))
+                .spawn();
+
+            let player_mesh = Entity::new()
+                .with_merge(make_transformable())
+                .with(model_from_url(), asset_url("assets/player.glb").unwrap())
+                .with(translation(), vec3(39., 39., 1.))
+                .with(scale(), Vec3::ONE * 0.5)
                 .spawn();
 
             entity::add_components(
                 id,
                 make_voxel()
                     .with_merge(make_transformable())
-                    .with_default(translation())
-                    .with_default(cube())
+                    .with(translation(), vec3(39., 39., 1.))
+                    // .with_default(cube())
                     .with(player_camera_ref(), camera)
                     .with(player_follower(), vec3(50.,50.,50.))
-                    .with(color(), vec4(0., 0.9, 0., 1.))
+                    .with(player_mesh_ref(), player_mesh)
+                    .with(has_gravity(), true)
+                    .with(color(), vec4(0.1, 0.2, 1., 1.))
             );
         }
     });
@@ -133,29 +141,35 @@ pub async fn main() -> EventResult {
             world.set(voxel_position, None);
             world.set(destination_request, Some(voxel_id));
 
-            if world.is_available(destination_request- Vec3::Z) {
-                entity::set_component(voxel_id, voxel_update(), -Vec3::Z)
+            let has_gravity = entity::get_component(voxel_id, has_gravity()).unwrap();
+            if has_gravity {
+                if world.is_available(destination_request - Vec3::Z) {
+                    entity::set_component(voxel_id, voxel_update(), -Vec3::Z)
+                }
             }
         }
     });
 
-    query((player(), timer(), player_camera_ref()))
+    query((player(), timer(), player_camera_ref(), player_mesh_ref()))
     .build()
     .each_frame(move |players| {
-        for (player_id, (_, timer_id, camera_id)) in players {
+        for (player_id, (_, timer_id, camera_id, mesh_id)) in players {
             let Some((delta, pressed)) = player::get_raw_input_delta(player_id) else { continue; };
 
             let timer_duration = entity::get_component(player_id, timer()).unwrap();
             let delay = 0.075;
-            let speed = 0.1;
 
+            // If the game has been running for less than 3 seconds, make the camera move slowly
             let mut follower_speed = 0.15;
             if time() < 3. {
                 follower_speed = 0.015;
             }
+
+
             let player_pos = entity::get_component(player_id, translation()).unwrap();
             let camera_pos = entity::get_component(camera_id, translation()).unwrap();
             let follower_pos = entity::mutate_component(player_id, player_follower(), |f| *f = f.lerp(player_pos, follower_speed)).unwrap();
+            entity::set_component(mesh_id, translation(), player_pos);
 
             // Get Camera Y Rotation
             let new_camera_yaw = entity::mutate_component(camera_id, player_camera_yaw(), |yaw| {
@@ -186,6 +200,7 @@ pub async fn main() -> EventResult {
             ].iter() {
                 let translate = || {
                     entity::set_component(player_id, voxel_update(), player_z_rotation.mul_vec3(*direction));
+                    entity::set_component(mesh_id, rotation(), Quat::from_rotation_z((player_direction / std::f32::consts::FRAC_PI_2).round() * std::f32::consts::FRAC_PI_2));
                 };
                 if delta.keys.contains(keycode) {
                     translate();
