51dd74d31a624b8637ac571282eed979b7065c75
Owen Palmer <speedcubegeek@gmail.com>
2023-03-29 18:50:58 -0700

    Add Greg with 3 animations

--- src/server.rs
+++ src/server.rs
@@ -2,8 +2,9 @@ use ambient_api::{
     global::{time, frametime, Quat},
     components::core::{
         app::main_scene,
-        camera::aspect_ratio_from_window,
-        model::{model_from_url},
+        camera::{aspect_ratio_from_window, fog},
+        prefab::prefab_from_url,
+        model::model_from_url,
         player::{player, user_id},
         primitives::{cube},
         transform::{lookat_center, translation, rotation, scale},
@@ -15,12 +16,13 @@ use ambient_api::{
     },
     player::KeyCode,
     concepts::{make_transformable, make_perspective_infinite_reverse_camera},
+    entity::{AnimationAction, AnimationController},
     prelude::*,
 };
-use components::{world_ref, voxel_world, voxel, player_camera_ref, player_camera_yaw, jumping, jump_timer};
+use components::{world_ref, voxel_world, voxel, player_camera_ref, player_camera_pitch, player_camera_yaw, jumping, jump_timer, current_animation};
 
 #[main]
-pub async fn main() -> EventResult {
+pub async fn main() -> ResultEmpty {
     // For now, the world is only one chunk
     let world_id = Entity::new().with(voxel_world(), vec![]).spawn();
 
@@ -70,13 +72,17 @@ pub async fn main() -> EventResult {
     }
 
     entity::set_component(world_id, voxel_world(), voxels_list);
+    make_transformable().with_default(sun()).spawn();
+    make_transformable().with_default(sky()).spawn();
 
     spawn_query((player(), user_id())).bind(move |players| {
         for (id, (_, user)) in players {
             let camera = Entity::new()
                 .with_merge(make_perspective_infinite_reverse_camera())
                 .with(aspect_ratio_from_window(), EntityId::resources())
+                .with_default(player_camera_pitch())
                 .with_default(player_camera_yaw())
+                .with_default(fog())
                 .with(translation(), vec3(30., 25., 10.))
                 .with_default(main_scene())
                 .with(lookat_center(), vec3(0., 0., 0.))
@@ -88,7 +94,11 @@ pub async fn main() -> EventResult {
                     .with_merge(make_transformable())
                     .with_default(jumping())
                     .with_default(jump_timer())
-                    .with(model_from_url(), asset_url("assets/player.glb").unwrap())
+                    .with_default(current_animation())
+                    .with(
+                        prefab_from_url(),
+                        asset::url("assets/greg.fbx").unwrap(),
+                    )
                     .with(scale(), Vec3::ONE * 0.5)
                     .with_default(cube())
                     .with(player_camera_ref(), camera)
@@ -107,27 +117,28 @@ pub async fn main() -> EventResult {
     .each_frame(move |players| {
         for (player_id, (_, camera_id)) in players {
             let Some((delta, pressed)) = player::get_raw_input_delta(player_id) else { continue; };
-            let speed = 0.1;
+            let speed = 0.2;
 
             let player_pos = entity::get_component(player_id, translation()).unwrap();
             let camera_pos = entity::get_component(camera_id, translation()).unwrap();
             let camera_zoom = 10.0;
 
             // Get Camera Y Rotation
-            let new_camera_yaw = entity::mutate_component(camera_id, player_camera_yaw(), |yaw| {
-                let new = *yaw + (delta.mouse_position.y / 300.);
+            let camera_pitch = entity::mutate_component(camera_id, player_camera_pitch(), |pitch| {
+                let new = *pitch + (delta.mouse_position.y / 300.);
                 if new > -0.7 && new < 0.7 {
-                    *yaw = new;
+                    *pitch = new;
                 }
             }).unwrap();
 
-            let player_x_rotation = Quat::from_rotation_x(new_camera_yaw);
+            let camera_pitch_rotation = Quat::from_rotation_x(camera_pitch);
 
             // Get Camera Z Rotation
-            let player_z_rotation = entity::mutate_component(player_id, rotation(), |p| *p *= Quat::from_rotation_z(delta.mouse_position.x / 150.)).unwrap();
+            let camera_yaw = entity::mutate_component(camera_id, player_camera_yaw(), |p| *p += delta.mouse_position.x / 150.).unwrap();
+            let camera_yaw_rotation = Quat::from_rotation_z(camera_yaw);
 
             // Apply rotations
-            let new_camera_offset = player_z_rotation.mul_vec3(player_x_rotation.mul_vec3(Vec3::Y * camera_zoom));
+            let new_camera_offset = camera_yaw_rotation.mul_vec3(camera_pitch_rotation.mul_vec3(Vec3::Y * camera_zoom));
             entity::set_component(camera_id, translation(), player_pos + new_camera_offset);
 
             entity::set_component(camera_id, lookat_center(), player_pos);
@@ -157,11 +168,45 @@ pub async fn main() -> EventResult {
                 (&KeyCode::D, Vec3::X),
             ].iter() {
                 if pressed.keys.contains(keycode) {
-                    physics::move_character(player_id, player_z_rotation.mul_vec3(*direction).normalize_or_zero() * speed, 0.01, frametime());
+                    physics::move_character(player_id, camera_yaw_rotation.mul_vec3(*direction).normalize_or_zero() * speed, 0.01, frametime());
+                    entity::set_component(player_id, rotation(), camera_yaw_rotation);
+                }
+                if delta.keys.contains(keycode) {
+                    entity::set_component(player_id, current_animation(), String::from("Running"));
+                    entity::set_animation_controller(
+                        player_id,
+                        AnimationController {
+                            actions: &[AnimationAction {
+                                clip_url: &asset::url("assets/greg.fbx/animations/Running.anim").unwrap(),
+                                looping: true,
+                                weight: 1.,
+                            }],
+                            apply_base_pose: false,
+                        },
+                    );
                 }
             }
+
+            if !pressed.keys.contains(&KeyCode::W) && !pressed.keys.contains(&KeyCode::A) && !pressed.keys.contains(&KeyCode::S) && !pressed.keys.contains(&KeyCode::D) {
+                if entity::get_component(player_id, current_animation()).unwrap() != String::from("Idle") {
+                    entity::set_component(player_id, current_animation(), String::from("Idle"));
+                    entity::set_animation_controller(
+                        player_id,
+                        AnimationController {
+                            actions: &[AnimationAction {
+                                clip_url: &asset::url("assets/greg.fbx/animations/Idle.anim").unwrap(),
+                                looping: true,
+                                weight: 1.,
+                            }],
+                            apply_base_pose: false,
+                        },
+                    );
+
+                }
+            } 
+
         }
     });
 
-    EventOk
+    OkEmpty
 }
